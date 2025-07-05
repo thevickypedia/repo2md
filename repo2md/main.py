54947bba4d7759edf2a2be1aa5d47dced7658615
dormant-user <svignesh1793@gmail.com>
2025-07-05 10:50:25 -0500

    Replace extensions with language filter
    Add options to ignore files and directories
    
    Make markdown the default option
    
    Add extensive list of languages

--- repo2md/main.py
+++ repo2md/main.py
@@ -1,10 +1,15 @@
 import os
 import pathlib
 from collections.abc import Generator
-from typing import Dict, List, Sequence, Tuple
+from typing import Dict, List, Tuple
 
 from repo2md.tree import Tree
-from repo2md.utils import LOGGER
+from repo2md.utils import (
+    IGNORE_DIRECTORIES,
+    IGNORE_FILES,
+    LANGUAGE_EXTENSIONS,
+    LOGGER,
+)
 
 
 def get_content(filepath: str) -> Dict[str, str]:
@@ -19,27 +24,30 @@ def get_content(filepath: str) -> Dict[str, str]:
     """
     LOGGER.info("Reading file %s", filepath)
     with open(filepath) as fstream:
-        return {
-            filepath: "\n".join([f.replace("\n", "") for f in fstream.readlines()])
-        }
+        return {filepath: "\n".join([f.replace("\n", "") for f in fstream.readlines()])}
 
 
 def get_current(
-    dir_path: str, extensions: Sequence[str] = None
+    dir_path: str, ignore_files: List[str], language: str = None
 ) -> Generator[Dict[str, str]]:
     """Loops through the current directory and yields file contents as dictionaries.
 
     Args:
         dir_path: Directory path to loop through.
-        extensions: Sequence of file extensions to filter files.
+        ignore_files: Files to ignore in the directory.
+        language: Language to filter files.
 
     Yields:
         Dict[str, str]:
         A dictionary with the file path as the key and the file content as the value.
     """
     LOGGER.info("Reading directory %s", dir_path)
+    extensions = LANGUAGE_EXTENSIONS[language.lower()]
     for file in os.listdir(dir_path):
-        if extensions and pathlib.Path(file).suffix not in extensions:
+        if file.lower() in ignore_files:
+            LOGGER.debug("Ignoring file %s in directory %s", file, dir_path)
+            continue
+        if language and pathlib.Path(file).suffix not in extensions:
             continue
         filepath = os.path.join(dir_path, file)
         if os.path.isfile(filepath):
@@ -48,13 +56,17 @@ def get_current(
 
 def get_files(
     src: str,
-    extensions: Sequence[str] = None,
+    ignore_directories: List[str] = IGNORE_DIRECTORIES,
+    ignore_files: List[str] = IGNORE_FILES,
+    language: str = None,
 ) -> Generator[Tuple[str, List[Dict[str, str]]]]:
     """Walks through the source directory and yields directories with their file contents.
 
     Args:
         src: Source directory to walk through.
-        extensions: Extensions of files to include in the output.
+        ignore_directories: Directories to ignore in the walk.
+        ignore_files: Files to ignore in the directories.
+        language: Language to filter files.
 
     Yields:
         Tuple[str, List[Dict[str, str]]]:
@@ -62,23 +74,26 @@ def get_files(
     """
     LOGGER.info("Walking directory %s", src)
     for __path, directories, files in os.walk(src):
+        if any((ignore in __path.lower() for ignore in ignore_directories)):
+            LOGGER.debug("Ignoring directory %s", __path)
+            continue
         # Loops through root directory
         if __path == src:
-            yield src, list(get_current(src, extensions))
+            yield src, list(get_current(src, ignore_files, language))
         # Loops through subdirectories
         for directory in directories:
+            if directory.lower() in ignore_directories:
+                LOGGER.debug("Ignoring directory %s", directory)
+                continue
             dir_path = os.path.join(__path, directory)
-            yield dir_path, list(get_current(dir_path, extensions))
+            yield dir_path, list(get_current(dir_path, ignore_files, language))
 
 
-def get_code(
-    iterator: Generator[Tuple[str, List[Dict[str, str]]]], markdown: bool = False
-) -> str:
+def get_code(iterator: Generator[Tuple[str, List[Dict[str, str]]]]) -> str:
     """Generates a formatted string of code contents from the iterator.
 
     Args:
         iterator: Iterator yielding directory paths and lists of file contents.
-        markdown: Boolean flag to format the output as Markdown.
 
     Returns:
         str:
@@ -88,31 +103,29 @@ def get_code(
     for __dir, content_list in iterator:
         for file_content_map in content_list:
             for filepath, content in file_content_map.items():
-                text += f"###### {filepath}\n\n" if markdown else f"-- {filepath}\n\n"
-                text += f"```\n{content.strip()}\n```" if markdown else content.strip()
+                text += f"###### {filepath}\n\n"
+                text += f"```\n{content.strip()}\n```"
                 text += "\n\n"
     return text
 
 
 def generate_markdown(
-    path: str | pathlib.Path | os.PathLike,
-    filename: str = None,
-    extensions: Sequence[str] = None,
+    path: str | pathlib.Path | os.PathLike, filename: str, language: str = None
 ) -> None:
     """Generates a Markdown file with the directory tree and code contents.
 
     Args:
         path: Path to the directory to process.
         filename: Filename for the output Markdown file.
-        extensions: Sequence of file extensions to filter files.
+        language: Programming language of the code files.
     """
     if not isinstance(path, pathlib.Path):
         path = pathlib.Path(path)
     assert path.exists(), f"Path {path.name} at {path.parent} does not exist"
     if not filename:
         filename = f"{path.name}.md"
-    iterator = get_files(str(path), extensions=extensions)
-    text = get_code(iterator, markdown=True)
+    iterator = get_files(str(path), language=language)
+    text = get_code(iterator)
     LOGGER.info("Generating tree for %s", path)
     tree = Tree(path).get()
     LOGGER.info("Storing output in %s", filename)
--- repo2md/utils.py
+++ repo2md/utils.py
@@ -1,6 +1,90 @@
 import logging
 import os
 
+LANGUAGE_EXTENSIONS = {
+    "python": [".py"],
+    "javascript": [".js", ".jsx"],
+    "typescript": [".ts", ".tsx"],
+    "java": [".java"],
+    "csharp": [".cs"],
+    "c++": [".cpp", ".h"],
+    "c": [".c", ".h"],
+    "ruby": [".rb"],
+    "go": [".go"],
+    "php": [".php"],
+    "html": [".html", ".htm"],
+    "css": [".css"],
+    "bash": [".sh"],
+    "kotlin": [".kt", ".kts"],
+    "swift": [".swift"],
+    "rust": [".rs"],
+    "scala": [".scala"],
+    "perl": [".pl"],
+    "lua": [".lua"],
+    "dart": [".dart"],
+    "elixir": [".ex", ".exs"],
+    "haskell": [".hs"],
+    "shell": [".sh", ".bash"],
+    "powershell": [".ps1"],
+    "sql": [".sql"],
+    "pascal": [".pas"],
+    "objective-c": [".m", ".h"],
+    "r": [".r"],
+    "julia": [".jl"],
+}
+
+IGNORE_FILES = [
+    ".gitignore",
+    "readme.md",
+    "license",
+    "requirements.txt",
+    "setup.py",
+    "pyproject.toml",
+    "pipfile",
+    "pipfile.lock",
+    "package.json",
+    "yarn.lock",
+    ".dockerignore",
+    ".pre-commit-config.yaml",
+    "makefile",
+    "settings.gradle",
+    "build.gradle",
+    "pom.xml",
+    "build.sbt",
+    "cargo.toml",
+    "cargo.lock",
+    "gradlew",
+    "gradlew.bat",
+    ".gitattributes",
+    "changelog.md",
+    ".java-version",
+    "openapi.yml",
+    "openapi.yaml",
+    "openapi.json",
+    "swagger.yml",
+    "swagger.yaml",
+    "swagger.json",
+]
+
+IGNORE_DIRECTORIES = [
+    "__pycache__",
+    ".git",
+    ".idea",
+    "venv",
+    ".github",
+    "node_modules",
+    ".ds_store",
+    ".vscode",
+    ".pytest_cache",
+    "build",
+    "dist",
+    "docs",
+    "tests",
+    "gradle",
+]
+
+IGNORE_LIST = IGNORE_DIRECTORIES + IGNORE_FILES
+
 LOGGER = logging.getLogger(__name__)
 handler = logging.StreamHandler()
 handler.setFormatter(
